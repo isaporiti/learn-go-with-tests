f98114a58ee66cca2956ffee9fb9918bebcde198
isaporiti <ignacio.saporiti@coduranc.com>
2021-12-04 19:50:21 +0100

    Extracts to functions

--- repeat/repeat.go
+++ repeat/repeat.go
@@ -3,13 +3,22 @@ package repeat
 const defaultRepeatTimes = 5
 
 func Repeat(character string, times int) string {
-	repeatTimes := defaultRepeatTimes
-	if times > 0 {
-		repeatTimes = times
-	}
+	repeatTimes := getRepeatTimes(times)
+	return repeat(character, repeatTimes)
+}
+
+func repeat(character string, repeatTimes int) string {
 	repeated := ""
 	for i := 0; i < repeatTimes; i++ {
 		repeated += character
 	}
 	return repeated
+}
+
+func getRepeatTimes(times int) int {
+	repeatTimes := defaultRepeatTimes
+	if times > 0 {
+		repeatTimes = times
+	}
+	return repeatTimes
 }
