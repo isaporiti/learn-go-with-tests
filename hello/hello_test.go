6e89e5f95243667799626eb463e2fbced6983726
isaporiti <ignacio.saporiti@coduranc.com>
2021-12-04 14:01:06 +0100

    Refactors test to reduce duplication

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,31 +3,25 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	expected := "Hello, World"
-
-	got := Hello("", "")
-
-	if got != expected {
-		t.Errorf("expected: %s, got: %s", expected, got)
+	type args struct {
+		name     string
+		language string
+		expected string
 	}
-}
-
-func TestHelloWithName(t *testing.T) {
-	expected := "Hello, Ignacio"
-
-	got := Hello("Ignacio", "")
-
-	if got != expected {
-		t.Errorf("expected: %s, got: %s", expected, got)
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "It greets with 'Hello, World' by default", args: args{name: "", language: "", expected: "Hello, World"}},
+		{name: "It greets with 'Hello, {name}' when passed a name", args: args{name: "Ignacio", language: "", expected: "Hello, Ignacio"}},
+		{name: "It greets with 'Hola, {name}' when passed 'Spanish' language", args: args{name: "Ignacio", language: "Spanish", expected: "Hola, Ignacio"}},
 	}
-}
-
-func TestHelloInSpanish(t *testing.T) {
-	expected := "Hola, Ignacio"
-
-	got := Hello("Ignacio", "Spanish")
-
-	if got != expected {
-		t.Errorf("expected: %s, got: %s", expected, got)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := Hello(test.args.name, test.args.language)
+			if test.args.expected != got {
+				t.Errorf("expected: %s, got: %s", test.args.expected, got)
+			}
+		})
 	}
 }
