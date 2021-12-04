520ccf2334e0d3a48aa2b48ff15b24b452eae158
isaporiti <ignacio.saporiti@coduranc.com>
2021-12-04 19:27:56 +0100

    Adds integer sum

---
+++ sum/sum_test.go
@@ -0,0 +1,26 @@
+package sum
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	type args struct {
+		x, y     int
+		expected int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "it sums 2 and 2", args: args{x: 2, y: 2, expected: 4}},
+		{name: "it sums 9 and 1", args: args{x: 9, y: 1, expected: 10}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var got int = Sum(test.args.x, test.args.y)
+
+			if test.args.expected != got {
+				t.Errorf("expected: %d, got: %d", test.args.expected, got)
+			}
+		})
+	}
+}
