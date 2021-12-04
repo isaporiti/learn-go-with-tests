4aab28fc9a3e3457e59f57287e8f4fc3f9ebeae6
isaporiti <ignacio.saporiti@coduranc.com>
2021-12-04 19:42:10 +0100

    Refactors test and adds real implementation

---
+++ repeat/repeat.go
@@ -0,0 +1,9 @@
+package repeat
+
+func Repeat(character string) string {
+	repeated := ""
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	return repeated
+}
--- repeat/repeat_test.go
+++ repeat/repeat_test.go
@@ -2,13 +2,24 @@ package repeat
 
 import "testing"
 
-func TestRepeat(t *testing.T) {
-	expected := "aaaaa"
-	if got := Repeat("a"); got != expected {
-		t.Errorf("expected: %s, got: %s", expected, got)
+func TestRepeat1(t *testing.T) {
+	type args struct {
+		character string
 	}
-}
-
-func Repeat(character string) string {
-	return "aaaaa"
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "repeats 'a' five times", args: args{character: "a"}, want: "aaaaa"},
+		{name: "repeats 'b' five times", args: args{character: "b"}, want: "bbbbb"},
+		{name: "repeats 'c' five times", args: args{character: "c"}, want: "ccccc"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Repeat(tt.args.character); got != tt.want {
+				t.Errorf("Repeat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
