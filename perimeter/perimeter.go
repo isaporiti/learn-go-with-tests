63eddc399573740d6a1274c03f2d734ce458def1
isaporiti <ignacio.saporiti@coduranc.com>
2021-12-25 17:33:21 +0100

    Adds Circle Area function

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,5 +1,7 @@
 package perimeter
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +11,14 @@ func Perimeter(r Rectangle) float64 {
 	return (r.Width + r.Height) * 2
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
 }
