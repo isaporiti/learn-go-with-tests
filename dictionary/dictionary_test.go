cd0ade578d51f0aec8121863d69837dc439e30b5
isaporiti <ignacio.saporiti@coduranc.com>
2022-01-01 19:37:06 +0100

    Adds Dictionary "add" method

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -29,5 +29,21 @@ func TestSearch(t *testing.T) {
 			t.Errorf("got %q want %q given, %q", got, want, "unknown")
 		}
 	})
+}
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
