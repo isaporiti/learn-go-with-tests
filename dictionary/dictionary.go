8679c924a23b27eeff3d8a3c5946e62d724e5429
isaporiti <ignacio.saporiti@coduranc.com>
2022-01-01 19:26:12 +0100

    Adds Dictionary Search
    - Unknown word error

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,14 @@
 package dictionary
 
+import "errors"
+
+type Dictionary map[string]string
+
 // Search looks up a word in a given dictionary and returns its definition if found.
-func Search(dictionary map[string]string, word string) (definition string) {
-	definition = dictionary[word]
-	return definition
+func (d Dictionary) Search(word string) (definition string, err error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you're looking for")
+	}
+	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -3,12 +3,28 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
 
-	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
-	}
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		if got != want {
+			t.Errorf("got %q want %q given, %q", got, want, "test")
+		}
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you're looking for"
+		got := err.Error()
+
+		if got != want {
+			t.Errorf("got %q want %q given, %q", got, want, "unknown")
+		}
+	})
+
 }
