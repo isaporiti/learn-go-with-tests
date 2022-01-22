078df6792baecb6cba8ec55f93fe634377508ccd
isaporiti <ignacio.saporiti@coduranc.com>
2022-01-22 20:51:38 +0100

    Prints countdown

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -8,5 +8,8 @@ import (
 // Countdown counts down from 3, printing each number on a new line (with a 1-second pause) and when it reaches zero it
 // prints "Go!"
 func Countdown(writer io.Writer) {
-	_, _ = fmt.Fprintf(writer, "3")
+	for i := 3; i > 0; i-- {
+		_, _ = fmt.Fprintln(writer, i)
+	}
+	_, _ = fmt.Fprint(writer, "Go!")
 }
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -11,8 +11,10 @@ func TestCountdown(t *testing.T) {
 	Countdown(buffer)
 
 	got := buffer.String()
-	want := "3"
-
+	want := `3
+2
+1
+Go!`
 	if want != got {
 		t.Errorf("want %q, got %q", want, got)
 	}
