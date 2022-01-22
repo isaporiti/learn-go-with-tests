1933f8fd2e49c83362b6c02f464830790a575396
isaporiti <ignacio.saporiti@coduranc.com>
2022-01-22 20:48:01 +0100

    Refactors

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -1,12 +1,12 @@
 package countdown
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
 // Countdown counts down from 3, printing each number on a new line (with a 1-second pause) and when it reaches zero it
 // prints "Go!"
-func Countdown(buffer *bytes.Buffer) {
-	_, _ = fmt.Fprintf(buffer, "3")
+func Countdown(writer io.Writer) {
+	_, _ = fmt.Fprintf(writer, "3")
 }
