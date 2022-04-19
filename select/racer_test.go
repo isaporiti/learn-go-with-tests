7e94ee276c6da3a07df15a81d6aa70bce1c31318
isaporiti <ignacio.saporiti@coduranc.com>
2022-04-19 20:49:58 +0200

    Makes test more reliable and fast

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,15 +1,27 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowUrl := "https://www.facebook.com"
-	fastUrl := "https://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		writer.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+	}))
 
-	got := Racer(slowUrl, fastUrl)
-	want := fastUrl
+	got := Racer(slowServer.URL, fastServer.URL)
+	want := fastServer.URL
 
 	if want != got {
 		t.Errorf("want %s, got %s", want, got)
 	}
+	slowServer.Close()
+	fastServer.Close()
 }
