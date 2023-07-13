f922535304862e257bac29c05fc95358b82868cb
Ignacio Saporiti <ignacio@saporiti.dev>
2023-07-13 11:22:23 +0200

    fixes get league from fs N times

--- server/store/file_system_store_test.go
+++ server/store/file_system_store_test.go
@@ -32,13 +32,40 @@ func TestFileSystemStore(t *testing.T) {
 			t.Errorf("got league: %v, want: %v", got, want)
 		}
 	})
+
+	t.Run("get league multiple times", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Pepper", "Wins": 2},
+			{"Name": "Floyd", "Wins": 3}
+		]`)
+		store := FileSystemStore{database}
+		var err error
+		want := []server.Player{
+			{Name: "Pepper", Wins: 2},
+			{Name: "Floyd", Wins: 3},
+		}
+
+		_, err = store.GetLeague()
+		if err != nil {
+			t.Fatal(err)
+		}
+		got, err := store.GetLeague()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got league: %v, want: %v", got, want)
+		}
+	})
 }
 
 type FileSystemStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (s *FileSystemStore) GetLeague() ([]server.Player, error) {
+	s.database.Seek(0, 0)
 	var league []server.Player
 	err := json.NewDecoder(s.database).Decode(&league)
 	if err != nil {
